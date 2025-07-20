7c234f029cc2561205d441ddc2b70c412a966478
Ruhul Amin <ruhulthisis@gmail.com>
2025-07-21 00:41:57 +0600

    Remove unused dynamic route components for cleanup

--- app/[...slug]/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Dpage = () => {
-  return (
-    <div>Dpage akhane route structure dynamic hoi  </div>
-  )
-}
-
-export default Dpage
--- app/[[...slug]]/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DDpage = () => {
-  return (
-    <div>Dpage akhane route structure dynamic hoi abaro hoi  </div>
-  )
-}
-
-export default DDpage
